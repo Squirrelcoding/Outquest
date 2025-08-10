19a5c1df879204bfe8428c55ed2268c2f99edd3e
Squirrelcoding <rosilescarlos28@gmail.com>
2025-08-09 22:02:40 -0500

    query works

--- queries/find_duplicates.sql
+++ queries/find_duplicates.sql
@@ -1,10 +1,32 @@
-create or replace function get_search_results(radius_meters float, current_lat float, current_long float) 
-returns table (id public.quest.id%TYPE, lat float, long float, distance_meters float8)
-set search_path = ''
+create or replace function get_search_results(
+  radius_meters float,
+  current_lat float,
+  current_long float
+) 
+returns table (
+  id public.quest.id%TYPE, 
+  lat float, 
+  long float, 
+  distance_meters float8
+)
 language sql
 as $$
-  select id, place, gis.st_y(location::gis.geometry) as lat, gis.st_x(location::gis.geometry) as long, gis.st_distance(location, gis.st_point(current_long, current_lat)::gis.geometry) as dist_meters
-  from public.cities
-  where gis.st_within(location::gis.geometry, gis.st_point(current_long, current_lat)::gis.geometry, radius_meters)
-  order by location operator(gis.<->) gis.st_point(long, lat)::gis.geography;
-$$
+  select 
+    quest.id, 
+    gis.st_y(cities.location::gis.geometry) as lat, 
+    gis.st_x(cities.location::gis.geometry) as long, 
+    gis.st_distance(
+      cities.location::gis.geography, 
+      gis.st_setsrid(gis.st_point(current_long, current_lat), 4326)::gis.geography
+    ) as distance_meters
+  from public.quest
+  inner join public.cities
+  on
+  cities.place = quest.location
+  where gis.st_dwithin(
+    cities.location::gis.geography, 
+    gis.st_setsrid(gis.st_point(current_long, current_lat), 4326)::gis.geography, 
+    radius_meters
+  )
+  order by distance_meters;
+$$;
